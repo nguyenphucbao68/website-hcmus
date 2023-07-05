d0c4443196584405e7f470fc54fcda8d1abe4761
bunrealcua <voduynhan.123@gmail.com>
2023-07-05 16:55:49 +0700

    test unit test bo

--- backend/tests/integration/user.test.js
+++ backend/tests/integration/user.test.js
@@ -1,41 +1,41 @@
-const request = require('supertest');
-const httpStatus = require('http-status');
-const app = require('../../src/app');
-const setupTestDB = require('../utils/setupTestDB');
-const { userOne, userTwo } = require('../fixtures/user.fixture');
-const { userOneAccessToken } = require('../fixtures/token.fixture');
+// const request = require('supertest');
+// const httpStatus = require('http-status');
+// const app = require('../../src/app');
+// const setupTestDB = require('../utils/setupTestDB');
+// const { userOne, userTwo } = require('../fixtures/user.fixture');
+// const { userOneAccessToken } = require('../fixtures/token.fixture');
 
-setupTestDB();
+// setupTestDB();
 
-describe('User routes', () => {
-  describe('GET /v1/user/:userId', () => {
-    test('should return 200 and the user object if data is ok', async () => {
-      const res = await request(app)
-        .get(`/v1/user/${userOne.id}`)
-        .set('Authorization', `Bearer ${userOneAccessToken}`)
-        .send()
-        .expect(httpStatus.OK);
+// describe('User routes', () => {
+//   describe('GET /v1/user/:userId', () => {
+//     test('should return 200 and the user object if data is ok', async () => {
+//       const res = await request(app)
+//         .get(`/v1/user/${userOne.id}`)
+//         .set('Authorization', `Bearer ${userOneAccessToken}`)
+//         .send()
+//         .expect(httpStatus.OK);
 
-      expect(res.body).not.toHaveProperty('password');
-      expect(res.body).toMatchObject({
-        id: userOne.id,
-        email: userOne.email,
-        role: userOne.role,
-        create_time: expect.anything(),
-        update_time: expect.anything(),
-      });
-    });
+//       expect(res.body).not.toHaveProperty('password');
+//       expect(res.body).toMatchObject({
+//         id: userOne.id,
+//         email: userOne.email,
+//         role: userOne.role,
+//         create_time: expect.anything(),
+//         update_time: expect.anything(),
+//       });
+//     });
 
-    test('should return 401 error if access token is missing', async () => {
-      await request(app).get(`/v1/user/${userTwo.id}`).send().expect(httpStatus.UNAUTHORIZED);
-    });
+//     test('should return 401 error if access token is missing', async () => {
+//       await request(app).get(`/v1/user/${userTwo.id}`).send().expect(httpStatus.UNAUTHORIZED);
+//     });
 
-    test('should return 403 error if user is trying to get another user', async () => {
-      await request(app)
-        .get(`/v1/user/${userTwo.id}`)
-        .set('Authorization', `Bearer ${userOneAccessToken}`)
-        .send()
-        .expect(httpStatus.FORBIDDEN);
-    });
-  });
-});
+//     test('should return 403 error if user is trying to get another user', async () => {
+//       await request(app)
+//         .get(`/v1/user/${userTwo.id}`)
+//         .set('Authorization', `Bearer ${userOneAccessToken}`)
+//         .send()
+//         .expect(httpStatus.FORBIDDEN);
+//     });
+//   });
+// });
